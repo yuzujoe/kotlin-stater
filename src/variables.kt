cc96acd7abc24dbd9243da3bf17470faf1bafb9d
yuzujoe <yuzun225@gmail.com>
2020-01-04 19:35:32 +0900

    variables

---
+++ src/variables.kt
@@ -0,0 +1,35 @@
+//Read-only local variables are defined using the keyword val. They can be assigned a value only once.
+fun readOnlyVal() {
+    val a: Int = 1  // immediate assignment
+    val b = 2   // `Int` type is inferred
+    val c: Int  // Type required when no initializer is provided
+    c = 3       // deferred assignment
+    println("a = $a, b = $b, c = $c")
+}
+
+//Variables that can be reassigned use the var keyword:
+fun reassginVal() {
+    var x = 5 // `Int` type is inferred
+    x += 1
+    println("x = $x")
+}
+
+// Top Level variables
+fun topLevelVal() {
+    var pi = 3.14
+    var x = 0
+
+    fun incrementX() {
+        x += 1
+    }
+
+    println("x = $x; PI = $pi")
+    incrementX()
+    println("x = $x; PI = $pi")
+}
+
+fun main() {
+    readOnlyVal()
+    reassginVal()
+    topLevelVal()
+}
