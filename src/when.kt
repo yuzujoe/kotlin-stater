d2de732459c4fe8daccb61aab063d080e3fb3f87
yuzujoe <yuzun225@gmail.com>
2020-01-05 17:34:51 +0900

    kotlin

---
+++ src/when.kt
@@ -0,0 +1,17 @@
+fun describe(obj: Any): String {
+    return when (obj) {
+        1          -> "One"
+        "Hello" -> "Greeting"
+        is Long -> "Long"
+        !is String -> "Not a string"
+        else       -> "Unknown"
+    }
+}
+
+fun  main() {
+    println(describe(1))
+    println(describe("Hello"))
+    println(describe(1000L))
+    println(describe(2))
+    println(describe("other"))
+}
