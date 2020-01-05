a1a73f080cd795ce6fac718d23ebe903d8a56df4
yuzujoe <yuzun225@gmail.com>
2020-01-05 15:49:41 +0900

    string templates

---
+++ src/string_templates.kt
@@ -0,0 +1,13 @@
+import kotlin.text.replace as replace1
+
+fun main() {
+    var a = 1
+    // simple name in template:
+    val s1 = "a is $a"
+    println(s1)
+
+    a = 2
+    // arbitrary expression in template:
+    val s2 = "${s1.replace1("is", "was")}, but now is $a"
+    println(s2)
+}
