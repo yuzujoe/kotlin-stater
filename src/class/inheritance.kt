6391bdae456d888351dc21e8512e3bc14c42dd21
yuzujoe <yuzun225@gmail.com>
2020-01-25 09:58:00 +0900

    override method

--- src/class/inheritance.kt
+++ src/class/inheritance.kt
@@ -1,11 +1,12 @@
 package `class`
 // Class without open cannot be inherited
 open class Animal(val name: String) {
-    fun sleep() = print("$name sleeps")
+    open fun sleep() = print("$name sleeps")
 }
 //
 class Cat(name: String) : Animal(name) {
     fun mew() = print("mew!")
+    override fun sleep() = print("$name had a good sleep")
 }
 
 fun main() {
