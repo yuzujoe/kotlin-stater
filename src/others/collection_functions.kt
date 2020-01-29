f051361f949f5f3324cd0ee6ba3b9fdda096bfd9
yuzujoe <yuzun225@gmail.com>
2020-01-29 18:18:33 +0900

    forEach

--- src/others/collection_functions.kt
+++ src/others/collection_functions.kt
@@ -5,7 +5,8 @@ data class Fruit(val name: String, val price: Int)
 fun main() {
     val fruits = listOf(Fruit("apple", 100), Fruit("orange", 120), Fruit("grape", 400))
 //  The filter function selects only collections whose functions return true, and returns them as a new collection
-    println(fruits.filter { it.price <= 200 })
+    fruits.filter { it.price <= 200 }
+        .forEach { println("$it") }
 //  map function applies the passed function to each element of the collection to create another collection
     println(fruits.map { (it.price * 1.08).toInt() })
 //  map and filter functions can be combined
@@ -15,4 +16,8 @@ fun main() {
 
     println(fruits.maxBy { it.price })
     println(fruits.minBy { it.price })
+//  find is a nullable type
+    println(fruits.find { it.name == "orange" })
+
+    fruits.forEach { println("$it") }
 }
